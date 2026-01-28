b4c767e719406ad33768039b01135c6cfbc4d270
hare0413-blip <harenotikumori0413@gmail.com>
2026-01-29 00:20:43 +0900

    Update enigma.js

--- enigma.js
+++ enigma.js
@@ -1,26 +1,28 @@
-const A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const A = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
 
-const R1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-const R2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
-const R3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
-const REF = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+const R1 = "そてまひらけこあよつにをほもさぬむえなうりちれゆいのふせろやはきかすわとん";
+const R2 = "ぬをけやすそえみはにまよさつあらひこてちせなうとほりのんもむきふいれゆかろわしへたお";
+const R3 = "りえさこふつもわてのあむへしゆにきほとんはよろぬいかたせけまみそらうちれおやなす";
+const REF = A.split("").reverse().join("");
+
+const N = A.length;
 
 function invert(r){
-  const inv = Array(26);
-  for(let i=0;i<26;i++) inv[A.indexOf(r[i])] = A[i];
+  const inv = Array(N);
+  for(let i=0;i<N;i++) inv[A.indexOf(r[i])] = A[i];
   return inv.join("");
 }
 const R1I = invert(R1), R2I = invert(R2), R3I = invert(R3);
 
 function f(c, r, p){
-  const i = (A.indexOf(c) + p) % 26;
+  const i = (A.indexOf(c) + p) % N;
   const w = r[i];
-  return A[(A.indexOf(w) - p + 26) % 26];
+  return A[(A.indexOf(w) - p + N) % N];
 }
 function b(c, rI, p){
-  const i = (A.indexOf(c) + p) % 26;
+  const i = (A.indexOf(c) + p) % N;
   const w = rI[i];
-  return A[(A.indexOf(w) - p + 26) % 26];
+  return A[(A.indexOf(w) - p + N) % N];
 }
 
 function encChar(c, p1, p2, p3){
@@ -37,20 +39,25 @@ function encChar(c, p1, p2, p3){
 }
 
 function runEnigma(){
-  const text = document.getElementById("inputText").value.toUpperCase();
-  let p1 = A.indexOf(document.getElementById("pos1").value.toUpperCase());
-  let p2 = A.indexOf(document.getElementById("pos2").value.toUpperCase());
-  let p3 = A.indexOf(document.getElementById("pos3").value.toUpperCase());
-  if(p1<0)p1=0; if(p2<0)p2=0; if(p3<0)p3=0;
+  const text = document.getElementById("inputText").value;
+
+  let p1 = A.indexOf(document.getElementById("pos1").value);
+  let p2 = A.indexOf(document.getElementById("pos2").value);
+  let p3 = A.indexOf(document.getElementById("pos3").value);
+
+  if (p1 < 0) p1 = 0;
+  if (p2 < 0) p2 = 0;
+  if (p3 < 0) p3 = 0;
 
   let out = "";
-  for(const c of text){
+  for (const c of text) {
     out += encChar(c, p1, p2, p3);
-    if(A.includes(c)){
-      p1 = (p1 + 1) % 26;
-      if(p1===0) p2 = (p2 + 1) % 26;
-      if(p1===0 && p2===0) p3 = (p3 + 1) % 26;
+    if (A.includes(c)) {
+      p1 = (p1 + 1) % N;
+      if (p1 === 0) p2 = (p2 + 1) % N;
+      if (p1 === 0 && p2 === 0) p3 = (p3 + 1) % N;
     }
   }
+
   document.getElementById("output").textContent = out;
 }
